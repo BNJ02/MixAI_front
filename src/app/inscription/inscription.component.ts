ea52315a3e26ab9365b51039c371d8a2ab510dc8
Benjamin Lepourtois <benji372002@gmail.com>
2024-11-20 14:02:48 +0100

    Modifications borders sur Signin et Signup (statique et dynamique)

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -30,7 +30,7 @@ export class InscriptionComponent {
   ) {
     this.inscriptionForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
     });
