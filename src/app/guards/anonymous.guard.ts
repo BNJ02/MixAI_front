f49d270ec59f37022b250258514a2fef895991aa
Benjamin Lepourtois <benji372002@gmail.com>
2024-11-22 14:13:41 +0100

    Fix pb d'anonymous guard

--- src/app/guards/anonymous.guard.ts
+++ src/app/guards/anonymous.guard.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnonymousGuard implements CanActivate {
 
-  constructor(private authService: AuthGuard, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(): boolean {
-    if (this.authService.canActivate()) {
+    if (this.authService.isAuthenticated()) {
       this.router.navigate(['/chat']);
       return false;
+    } else {
+      return true;
     }
-    return true;
   }
 }
