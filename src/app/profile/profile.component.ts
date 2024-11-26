a621a4c4378fb92bd8774f78732e71017d71340e
Benjamin Lepourtois <benji372002@gmail.com>
2024-11-27 00:16:53 +0100

    Première ébauche de la page profile avec un peu d'interactions avec le back

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { User } from '../types/user.interface';
-import { combineLatest } from 'rxjs';
+import { combineLatest, finalize, take, tap } from 'rxjs';
 import { SigninSuccessService } from '../services/signinSuccess.service';
 import { SigninSuccess } from '../types/signinSuccess.interface';
+import { AlertComponent } from "../alert/alert.component";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, AlertComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
@@ -18,6 +19,8 @@ export class ProfileComponent {
   public profileForm: FormGroup;
   public signinSuccess: SigninSuccess;
   public user: User;
+  public loading = false;
+  public updateSuccess = false;
 
   constructor(
     private fb: FormBuilder,
@@ -25,18 +28,51 @@ export class ProfileComponent {
     private router: Router
   ) {
     this.profileForm = this.fb.group({
-      name: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      API_key: ['', Validators.required]
     });
   }
 
   public ngOnInit(): void {    
     this.userService.getMe()
-      .subscribe((user) => {
-        this.user = user;
-        this.profileForm.patchValue(user);
-      });
+      .pipe(
+        tap((user) => {
+          this.user = user
+          this.profileForm.patchValue(user);
+        })
+      )
+      .subscribe();
   }
 
+  public updateProfile(): void {
+    this.loading = true;
+    const userRequest: User = {
+      firstName: this.profileForm.get('firstName')?.value,
+      lastName: this.profileForm.get('lastName')?.value,
+      email: this.profileForm.get('email')?.value,
+    };
+    this.userService
+      .updateUser(userRequest)
+      .pipe(
+        take(1),
+        tap((res) => {
+          if (res) {
+            this.updateSuccess = true;
+            setTimeout(() => {
+              this.updateSuccess = false;
+            }, 3000);
+          }
+        }),
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe();
+  }
+
+  public backToChat(): void {
+    this.router.navigate(['/chat']);
+  }
 }
